f57c27c87d6a4ade07cf47580f3ea3050b348a2a
Gladox114 <53704522+Gladox114@users.noreply.github.com>
2023-11-15 13:06:51 +0100

    Create fakeTurtle.lua

---
+++ fakeTurtle.lua
@@ -0,0 +1,67 @@
+turtle = {}
+virtInv = {
+
+}
+
+function turtle.forward()
+
+end
+
+function turtle.up()
+
+end
+
+function turtle.down()
+
+end
+
+function turtle.back()
+
+end
+
+function turtle.turnLeft()
+
+end
+
+function turtle.turnRight() 
+
+end
+
+function turtle.select(number slotNum) 
+ 
+end
+
+-- will need a virtual inventory
+function turtle.getSelectedSlot() 
+
+end
+
+function turtle.getItemSpace([number slotNum]) 
+
+end
+
+function turtle.getItemDetail([number slotNum]) 
+
+end
+
+function turtle.getItemCount([number slotNum]) 
+
+end
+
+function turtle.dig([string toolSide]) 
+
+end
+
+function turtle.digUp([string toolSide]) 
+
+end
+
+function turtle.digDown([string toolSide]) 
+
+end
+
+function turtle.inspect()
+
+end
+
+
